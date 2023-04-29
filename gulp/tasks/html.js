49bf86c4910726559c89fd0771e3a9d03f35276b
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2023-04-29 22:55:17 +0700

    fix: поменял настройки для js файлов

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -7,6 +7,8 @@ module.exports = () => {
 		.pipe(plugins.plumber())
 		.pipe(plugins.fileInclude())
 		.pipe(plugins.replace(/@images\//g, "assets/images/"))
+		.pipe(plugins.replace(/@script\//g, "assets/script/"))
+		.pipe(plugins.replace(/js/g, "min.js"))
 		.pipe(plugins.htmlmin({ removeComments: true, collapseWhitespace: true }))
 		.pipe(plugins.gulp.dest(path.build.html));
 };
